0b6207bbe6aa75c7fc596f8ab154995bfed4856a
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2022-12-31 23:47:23 +0530

    ʙᴀʙʏ🥀

--- KRISTY/modules/source.py
+++ KRISTY/modules/source.py
@@ -2,37 +2,33 @@ from platform import python_version as y
 
 from pyrogram import __version__ as z
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram import __version__ as o
 from telethon import __version__ as s
 
-from KRISTY import OWNER_USERNAME, dispatcher
-from KRISTY import pbot as client
+from KRISTY import BOT_NAME, BOT_USERNAME, OWNER_USERNAME, OWNER_ID, START_IMG, pbot
 
 
-@client.on_message(filters.command(["repo", "source"]))
-async def repo(client, message):
-    await message.reply_sticker("CAACAgQAAxkBAAIC1mMgYzAPRWv7N_G5jP9rRAYOf7wrAALnCgACwDCxUU6M2FZ1FdW8KQQ")
+@pbot.on_message(filters.command(["repo", "source"]))
+async def repo(_, message: Message):
     await message.reply_photo(
-        photo="https://telegra.ph/file/6bcaa6aea22a15d620490.jpg",
-        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+        photo=START_IMG,
+        caption=f"""**ʜᴇʏ {message.from_user.mention},
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [ALONE](https://t.me/i_am_pro_king)
+ɪ ᴀᴍ [{BOT_NAME}](https://t.me/{BOT_USERNAME})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** [ᴀʟᴏɴᴇ](https://t.me/{OWNER_USERNAME})
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
-
-**KRISTY sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER_ID),
                     InlineKeyboardButton(
-                        "• ᴏᴡɴᴇʀ •", url=f"https://t.me/{OWNER_USERNAME}"
-                    ),
-                    InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •",
+                        "sᴏᴜʀᴄᴇ",
                         url="https://github.com/ALTRON-XD/KRISTY",
                     ),
                 ]
